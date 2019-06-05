0d4bb19d5f331ca3d1551e9187c06f5bcd584d8c
zhouhong <278111013@qq.com>
2019-06-05 14:51:50 +0800

    [add]添加一些资源

--- trivia.sh
+++ trivia.sh
@@ -5,43 +5,47 @@ FILE_TMP="${DOWNLOAD_PATH}/.temp"
 
 #下载资源
 function download(){
-  echo "客观稍等,下载资源......"
-  if [[ ! -e ${URLS} ]]; then
+echo "客观稍等,下载资源"
+if [[ ! -e ${URLS} ]]; then
+  #statements
+  echo "资源列表不存在"
+  exit 1
+fi
+exec <  ${URLS} 
+read URL
+while [[ $? -eq 0 ]]
+do 
+  #检查url的有效性
+  #echo "读取到一个网址 ${URL}"
+  #下载html文件
+   echo ".\c"
+  #`wget -P "${DOWNLOAD_PATH}" "${URL}" >>"${DOWNLOAD_PATH}/.log" 2>&1 `
+  `wget -P "${DOWNLOAD_PATH}" "${URL}" >/dev/null 2>&1`
+  read URL 
+done
+echo "download done"
+for file in  ${DOWNLOAD_PATH}/[^.]* ; do
+  #statements
+  #echo "文件 : $file"
+  if [[ -r $file && $file == *.html ]]; then
     #statements
-    echo "资源列表不存在"
-    exit 1
-  fi
-  exec <  ${URLS} 
-  read URL
-  while [[ $? -eq 0 ]]
-  do 
-    #检查url的有效性
-    #echo "读取到一个网址 ${URL}"
-    #下载html文件
-    `wget -P "${DOWNLOAD_PATH}" "${URL}" >>"${DOWNLOAD_PATH}/.log" 2>&1 `
-    read URL 
-  done
+    # echo "$file 满足条件"
 
-  for file in  ${DOWNLOAD_PATH}/[^.]* ; do
-    #statements
-    #echo "文件 : $file"
-    if [[ -r $file && $file == *.html ]]; then
-      #statements
-      # echo "$file 满足条件"
 
     #获取下载到的文件 筛选出信息 导入 临时文件中
     cat $file | grep "<p>\d\+\..*</p>" >> $FILE_TMP 
     #删除 <p> 标签
 
 
-    fi
-  done
+  fi
+done
 
 #去除标签信息
-sed -i ''  -e "s/<p>//g"  -e "s/<\/p>//g"  $FILE_TMP 
-sed -i '' "s/<a.*a>//g" $FILE_TMP
-sed -i '' "s/\[.*\]//g" $FILE_TMP
-sed -i '' "s/<div.*>//g" $FILE_TMP
+sed -i ''  \
+  -e "s/<p>//g"  -e "s/<\/p>//g" \
+  -e  "s/<a.*a>//g" -e "s/\[.*\]//g"  \
+  -e"s/<div.*>//g" -e  "s/^[0-9]\{1,10\}\.//g" \
+  $FILE_TMP 
 }
 
 function getOneMsg(){
@@ -68,23 +72,9 @@ echo '#】'
 
 
 }
-function checkTools(){
 
-  indicateHomeBrew=`command -v brew`
-  if [[ ! -x `command -v brew` ]]; then
-    echo "安装brew..."
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  fi
 
-  if [[ !  -x `command -v wget` ]]; then
-    #statements
-    echo "安装wget"
-    brew install wget
-  fi
 
-}
-
-checkTools
 if [[ -r $FILE_TMP ]]; then
   #statements
   getOneMsg 
